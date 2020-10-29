d1bded6ef8119d4ec40bab7933e7357b1a5f33b9
Darvvvin <jacobisdarvin@gmail.com>
2020-10-29 20:40:50 +0800

    Initial Publishing of UChan
    - Homepage Added, Node dependencies and NPM Inits

---
+++ index.js
@@ -0,0 +1,40 @@
+// Import
+const express 	= require('express');
+const exphbs 	= require('express-handlebars');
+const fs 		= require('fs');
+const hbs 		= require('hbs');
+const mongoose 	= require('mongoose');
+const path 		= require('path');
+
+const app 		= express();
+
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'hbs');
+app.engine(
+	'hbs',
+	exphbs({
+		extname: 		'hbs',
+		defaultView: 	'main',
+		layoutsDir: 	path.join(__dirname, '/views/layouts'),
+        partialsDir: 	path.join(__dirname, '/views/partials')
+	}),
+);
+
+const routes = require('./routes/routes.js');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use('/', routes);
+
+let port = process.env.PORT;
+
+if(port == null || port == "") {
+    port = 9090;
+}
+
+app.listen(port, function () {
+    console.log('UChan listening at port ' + port + '.');
+});
---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+// Import
+const express 	= require('express');
+const fs 		= require('fs');
+const hbs 		= require('hbs');
+const mongoose 	= require('mongoose');
+const path 		= require('path');
+
+const app 		= express();
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		title: 'Home',
+	});
+});
+
+module.exports = app;
