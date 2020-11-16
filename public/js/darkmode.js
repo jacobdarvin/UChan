c18e44e7202673a665707ae05ed3f8d6c9b834c2
Darvvvin <jacobisdarvin@gmail.com>
2020-11-16 18:45:43 +0800

    Leaf theme added. Team selecting UI is buggy

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -1,49 +1,79 @@
 let darkMode = localStorage.getItem('darkMode');
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
+let leafMode = localStorage.getItem('leafMode');
 
-//check if dm is enabled
-//if its enabled turn it off
-//if disabled turn it on!
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const leafModeToggle = document.querySelector('#leaf-mode-toggle');
+
+//FUNCTIONS
+
+//DARK MODE
 
 const enableDarkMode = () => {
-	//1. change css link
 	document.getElementById('theme').setAttribute('href', '/css/dark-style.css');
 	document.getElementById('navbar').classList.remove("navbar-light");
 	document.getElementById('navbar').classList.add("navbar-dark");
 
-
 	if(document.getElementById('logo'))
 		document.getElementById('logo').setAttribute('src',  'imgs/icons/logo-dark.png');
-	//2. update darkmode in the local storage
+
+	localStorage.setItem('leafMode', null);
 	localStorage.setItem('darkMode', 'enabled');
 };
 
-const disableDarkMode = () => {
-	//1. change css link
+// LEAF MODE
+
+const enableLeafMode = () => {
+	document.getElementById('theme').setAttribute('href', '/css/leaf-style.css');
+	document.getElementById('navbar').classList.remove("navbar-dark");
+	document.getElementById('navbar').classList.add("navbar-light");
+
+	if(document.getElementById('logo'))
+		document.getElementById('logo').setAttribute('src',  'imgs/icons/logo-leaf.png');
+
+	localStorage.setItem('darkMode', null);
+	localStorage.setItem('leafMode', 'enabled');
+};
+
+// THEMES DISABLER
+
+const disableThemes = () => {
 	document.getElementById('theme').setAttribute('href', '/css/style.css');
 	document.getElementById('navbar').classList.add("navbar-light");
 	document.getElementById('navbar').classList.remove("navbar-dark");
 
 	if(document.getElementById('logo'))
 		document.getElementById('logo').setAttribute('src',  'imgs/icons/logo.png');
-	//2. update darkmode in the local storage
+
 	localStorage.setItem('darkMode', null);
+	localStorage.setItem('leafMode', null);
 };
+
+//FUNCTIONS
 
 if(darkMode === 'enabled') {
 	enableDarkMode();
+} else if(leafMode === 'enabled') {
+	enableLeafMode();
 } else {
-	disableDarkMode();
+	disableThemes();
 }
+
+// EVENT LISTENERS
 
 darkModeToggle.addEventListener('click', () => {
 	darkMode = localStorage.getItem('darkMode');
-
 	if(darkMode !== 'enabled') {
 		enableDarkMode();
-		console.log(darkMode);
 	} else {
-		disableDarkMode();
-		console.log(darkMode);
+		disableThemes();
+	}
+});
+
+leafModeToggle.addEventListener('click', () => {
+	leafMode = localStorage.getItem('leafMode');
+	if(leafMode !== 'enabled') {
+		enableLeafMode();
+	} else {
+		disableThemes();
 	}
 })
