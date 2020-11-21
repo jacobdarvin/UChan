ad06d71bee031ea41ce740aa32eec4c832198df0
Darvvvin <jacobisdarvin@gmail.com>
2020-11-21 18:30:48 +0800

    Update darkmode.js

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -163,6 +163,6 @@ function addCounter() {
 	if(egg == 7) {
 		document.getElementById('logo-link').setAttribute('href',  '#eggvideo');
 		$('#logo-link').append('<iframe width="1004" id="eggvideo" height="753" src="https://www.youtube.com/embed/ByrCj6eMMqo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-		document.getElementById('logo-link').css("display: none;");
+		$("#logo").css("display", "none");
 	}
 }
