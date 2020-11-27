8e259675544e5c6b29bba7cc3479495917a6d8b1
Darvvvin <jacobisdarvin@gmail.com>
2020-11-27 22:00:03 +0800

    added delay to dissapearing

--- public/js/thread.js
+++ public/js/thread.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#thread").html(function(_, html){
-        return html.replace(/(&gt;.*)/g, '<span style="color: #789922">$1</span>');
+        return html.replace(/(&gt;.*)/g, '<span style="color: grey">$1</span>');
         
         //return html.replace(/(&gt;&gt;\w+)/g, '<span style="color: red">$1</span>');
         //To DO: Figure out how to extract quotes.
@@ -33,15 +33,22 @@ function toggleImage(img) {
     }
 }
 
+/* Reply Highlighting */
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function highlightReply(id) {
     let replyId = id + "p";
     var x = document.getElementById(replyId);
-    x.style.border = "2px solid #ff5757";
+    x.style.border = "2px dashed black";
 }
 
-function exitHighlight(id) {
+async function exitHighlight(id) {
     let replyId = id + "p";
     var x = document.getElementById(replyId);
-
+    
+    await sleep(2500);
     x.style.border = "none";
 }
