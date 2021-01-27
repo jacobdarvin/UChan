0d5815d5683b6a7b69fe24607c4c9065b1604a48
Justin <justingalura@gmail.com>
2021-01-27 21:47:48 +0800

    hook reportedposts datatable

--- controller/modController.js
+++ controller/modController.js
@@ -4,22 +4,19 @@ const getModView = async(req ,res) => {
 
     let reportedPosts = [];
     try {
-        reportedPosts = await ReportedPost.find().sort({date: 'desc'}).lean();
+        reportedPosts = await ReportedPost.find().sort({date: 'desc'})
+            .select('_id postNumber board date offTopicCounter lawCounter spamCounter').lean();
     } catch (e) {
         console.log(e);
         res.render('404', {title: '404'});
         return;
     }
 
-    //post no, board, date, postid, comment
-    for (let i = 0; i < reportedPosts.length; i++) {
-
-    }
-
     res.render('modview', {
         title: 'Moderator View',
         thread: false,
         about_active: true,
+        reportedPosts: reportedPosts
     });
 }
 
