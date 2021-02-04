93269e403985845135b70ffd26f4e528a7e4c5e7
Darvvvin <jacobisdarvin@gmail.com>
2021-02-05 01:53:24 +0800

    FASFSAFSA

--- controller/registerController.js
+++ controller/registerController.js
@@ -6,31 +6,30 @@ const sanitize = require('mongo-sanitize');
 const db = require('../model/database.js');
 
 //TODO: verify that user is not logged in and captcha
-const getRegister = async(req, res) => {
-    res.render('xeroxthat', {
-        title: 'XeroxThat',
-        thread: false,
-        about_active: true,
-    });
+const getRegister = async (req, res) => {
+  res.render('xeroxthat', {
+    title: 'XeroxThat',
+    thread: false,
+    about_active: true,
+  });
 }
 
-const postRegister = async(req, res) => {
-    let username = sanitize(req.body['id-register']);
-    let password = sanitize(req.body['password-register']);
-    let registerKey = sanitize(req.body['key-register']);
+const postRegister = async (req, res) => {
+  let username = sanitize(req.body['id-register']);
+  let password = sanitize(req.body['password-register']);
+  let registerKey = sanitize(req.body['key-register']);
 
-    //TODO: ajax in the future
-    let result = await userTransactor.createUser(username, password, registerKey);
-    if (!result.result) {
-        res.redirect(req.get('referer'));
-        return;
-    }
+  //TODO: ajax in the future
+  let result = await userTransactor.createUser(username, password, registerKey);
+  if (!result.result) {
+    res.redirect(req.get('referer'));
+    return;
+  }
 
-    res.redirect('/xeroxthis');
+  res.redirect('/xeroxthis');
 }
 
 module.exports = {
-    getRegister,
-    postRegister
+  getRegister,
+  postRegister
 };
-
