7ad24c9b9b80fcb311934194e9db37ce271622c6
Darvvvin <jacobisdarvin@gmail.com>
2021-02-05 01:39:25 +0800

    list and catalogue front end

--- public/js/uchan.boards.js
+++ public/js/uchan.boards.js
@@ -1,3 +1,4 @@
+/*
 $('#view-selector').change(function(){
   if($(this).val() == 'list') {
     $('#type-view').removeClass("catalogue-view");
@@ -7,5 +8,34 @@ $('#view-selector').change(function(){
       $('#type-view').removeClass("container");
       $('#type-view').addClass("catalogue-view");
     }
+  }
+});
+*/
+let viewChoice = localStorage.getItem('viewChoice');
+
+$( document ).ready(function() {
+    if(viewChoice === 'list') {
+      $("#view-selector").val("list");
+
+      $('#type-view').removeClass("catalogue-view");
+      $('#type-view').addClass("container");
+    } else {
+      $('#type-view').removeClass("container");
+      $('#type-view').addClass("catalogue-view");
+    }
+});
+
+$('#view-selector').change(function(){
+  var v = window.location.href;
+
+  if($(this).val() == 'list') {
+    localStorage.setItem('viewChoice', 'list');
+    k = v + '?' + 'list=true';
+
+    window.location.replace(k);
+
+  } else {
+    localStorage.setItem('viewChoice', 'catalogue');
+    window.location.replace(window.location.origin + window.location.pathname);
   }
 });
