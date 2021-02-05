7c50f613f5a8f5e2b41b67eadaf923c77650e337
Darvvvin <jacobisdarvin@gmail.com>
2021-02-05 23:36:43 +0800

    commit

--- public/js/uchan.datatables.js
+++ public/js/uchan.datatables.js
@@ -1,5 +1,31 @@
 $(document).ready( function () {
-    $('#reportTable').DataTable();
-    $('#moderatorTable').DataTable();
-    $('#bannedTable').DataTable();
+    var reportTable = $('#reportTable').DataTable();
+    var moderatorTable = $('#moderatorTable').DataTable();
+    var bannedTable = $('#bannedTable').DataTable();
+
+    //ON OPEN
+    $('#reportTable tbody').on('click', 'tr', function () {
+      var data = reportTable.row( this ).data();
+      //alert( 'You clicked on '+data[0]+'\'s row' );
+
+      $('#reportDataModal-Title').text("Post @" + data[0]);
+
+      $('#reportDataModal-PostN').text(data[0]);
+      $('#reportDataModal-Board').text(data[1]);
+      $('#reportDataModal-Law').text(data[2]);
+      $('#reportDataModal-Off').text(data[3]);
+      $('#reportDataModal-Spam').text(data[4]);
+
+      $('#reportDataModal').modal('show');
+    } );
+
+    $('#moderatorTable tbody').on('click', 'tr', function () {
+      var data = moderatorTable.row( this ).data();
+      alert( 'You clicked on '+data[0]+'\'s row' );
+    } );
+
+    $('#bannedTable tbody').on('click', 'tr', function () {
+      var data = bannedTable.row( this ).data();
+      alert( 'You clicked on '+data[0]+'\'s row' );
+    } );
 } );
