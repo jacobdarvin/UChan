6cb055cb8218ad66d1487547d2e14b9ace9c1ee1
Justin <justingalura@gmail.com>
2021-02-07 17:08:29 +0800

    Update adduserboard.js

--- adduserboard.js
+++ adduserboard.js
@@ -4,9 +4,8 @@ const User = require('./model/user.js');
 database.connectToDb();
 
 (async function() {
-    let user = await User.findOne({name: 'nigga'});
-    user.rank = 'ADMIN';
-    user.name = 'admin';
+    let user = await User.findOne({name: 'admin'});
+    user.boards = ['ufo', 'cas', 'vgs', 'fas', 'ccs', 'dls', 'inv'];
     //user['boards'].push('ufo');
     await user.save();
 })();
