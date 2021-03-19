26b4cfb3bf8b85d9ea4f03b501f85b8149c36358
Justin <justingalura@gmail.com>
2021-03-19 11:10:54 +0800

    more indexcontroller cleanup

--- controller/indexController.js
+++ controller/indexController.js
@@ -11,21 +11,34 @@ const userTransactor = require('../helper/user-transactor.js');
 // Controller Functions
 //======================================================================
 
+/**
+ * Invokes the home page containing the 8 most recently bumped threads.
+ * 
+ * @param {Request} req the request object
+ * @param {Response} res the response object
+ * 
+ * 
+ */
 const getIndex = async (req, res) => {
     await userTransactor.createUserCookie(req, res);
 
-    let activeThreads = await  Post.find({type: 'THREAD'})
-                    .sort({bump: 'desc'})
-                    .limit(8)
-                    .lean();
+    try {
+        const activeThreads = await  Post.find({type: 'THREAD'})
+                        .sort({bump: 'desc'})
+                        .limit(8)
+                        .lean();
 
-    res.render('index', {
-        active_session: req.session.user && req.cookies.user_sid,
-        title: 'UChan',
-        threads: activeThreads,
-        home_active: true,
-    });
-
+        res.render('index', {
+            active_session: req.session.user && req.cookies.user_sid,
+            title: 'UChan',
+            threads: activeThreads,
+            home_active: true,
+        });
+    } catch (e) {
+        console.log("Index controller getIndex: " + e);
+        //TODO: better error pages
+        res.render('404', {title: "Internal error."});
+    }
     return;
 }
 
